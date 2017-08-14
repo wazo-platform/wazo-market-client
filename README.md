b3522b4e0d4641da3d37969ea52d47f0807ea75f
Pascal Cadotte Michaud <pcm@wazo.community>
2017-08-14 10:51:15 -0400

    add the plugins.list method

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # wazo-market-client
 A python client for apps.wazo.community
+
+## Usage
+
+### Creating a client
+
+```python
+from wazo_market_client import Client
+c = Client()
+```
+
+### Listing available plugins
+
+```python
+c.plugins.list()
+```
---
+++ wazo_market_client/client.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from xivo_lib_rest_client.client import BaseClient
+
+
+class Client(BaseClient):
+
+    namespace = 'wazo_market_client.commands'
+
+    def __init__(self, host='apps.wazo.community', port=None, version='0.1', https=False, **kwargs):
+        super(Client, self).__init__(host=host, port=port, version=version, https=https, **kwargs)
